b7e717d9d7c81fea9a4ecde591eb380c46bf6bbc
YeissonOrtiz <ortizyeison2183@gmail.com>
2022-04-23 12:29:18 -0500

    Modified login form

--- loginForm.php
+++ loginForm.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="../css/styles.css">
-  <link rel="stylesheet" href="../css/loginFormStyles.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <link rel="stylesheet" href="./css/loginFormStyles.css">
 </head>
 <body>
   <header>
